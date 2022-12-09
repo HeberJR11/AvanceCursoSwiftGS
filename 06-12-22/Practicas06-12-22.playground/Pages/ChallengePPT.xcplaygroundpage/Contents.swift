6e83c540d3c1d4a09ac114f1dfc25bc3261cee53
HeberJR11 <heberedu_17@hotmail.com>
2022-12-08 21:42:17 -0600

    Avance semana1

---
+++ 06-12-22/Practicas06-12-22.playground/Pages/ChallengePPT.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Piedra-Papel-Tijeras
+
+"""
+Piedra = 1
+Papel = 2
+Tijeras = 3
+
+Perder = 0
+Empate = 3
+Ganar = 6
+Contrincante              Yo
+A(piedra) ------------- Y (papel)
+B(papel) ------------- X(piedra)
+C(tijeras) ------------- Z(tijeras)
+
+Sumar lo spuntos obtenidos en las tres rondas py determinar quien gana
+Usar diccionario
+"""
+
+var Diccionario: [String:String] = [:]
+var contrincnateArr = [1, 2, 3]
+var myArr = [2, 1, 3]
+
+for i in myArr {
+    print(i)
+}
+
+
+
---
+++ 07-12-22/Practicas07-12-22.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+"""
+Invertir la entrada
+Input
+4
+Hello World
+
+Output
+roH
+l e
+dWl
+ ol
+"""
+
+var columnas = 4
+var filas = 4
+var input = "Hello World"
+
+/*
+var arreglo = [[String]](repeating: [String](repeating:"Hola- ", count: columnas), count: filas)
+var cadena = ""
+for indice in arreglo{
+    cadena = ""
+    for valor in indice {
+        cadena += "\(valor)"
+    }
+    print(cadena)
+}
+*/
+
+for i in (0...3).reversed(){
+    //print(i)
+    for j in 0...3 {
+        
+    }
+}
+
